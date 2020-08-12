b8b57e8db331f27daee7a814215347a4704e8991
egroge <egroge.stacey@gmail.com>
2020-08-12 11:59:10 +0100

    Fixed clippy

--- src/ewasm/expressions.rs
+++ src/ewasm/expressions.rs
@@ -3,10 +3,13 @@ use crate::ast::Expression;
 use crate::ewasm::function_context::FunctionContext;
 use crate::ewasm::Codegen;
 
+// TODO remove this allowance when it gets used
+#[allow(dead_code)]
 pub struct EWASMExpression<'a> {
     pub expr: &'a Expression,
 }
 
+#[allow(dead_code)]
 impl<'a> EWASMExpression<'a> {
     // We want to take an expression, create any of the intermediary steps to evaluate it,
     // and then return the tmp variable that stores the evaluated result
