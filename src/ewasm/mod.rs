208ff6a07940f1ff11b8e77fbf7789e3e9379b08
egroge <egroge.stacey@gmail.com>
2020-09-10 15:29:37 +0100

    Removed hardcoded strings from file creation

--- src/ewasm/mod.rs
+++ src/ewasm/mod.rs
@@ -31,6 +31,11 @@ use std::{fs, path::Path, process};
 use wabt::wasm2wat;
 use wabt::wat2wasm;
 
+const JSON_LCC_KEY: &str = "llcPath";
+const JSON_WASM_LD_KEY: &str = "wasm-ldPath";
+const TEMPORARY_DIRECTORY_NAME: &str = "tmp";
+const OUTPUT_DIRECTORY_NAME: &str = "output";
+
 pub fn generate(module: &Module, context: &mut Context) {
     let external_traits = module
         .declarations
@@ -107,7 +112,7 @@ pub fn generate(module: &Module, context: &mut Context) {
 
     // It seems from the move target that we are allowed to create multiple contracts, and each of these
     // gets put into a separate file, so we will do the same here
-    let output_path = Path::new("output");
+    let output_path = Path::new(OUTPUT_DIRECTORY_NAME);
     if !output_path.exists() {
         fs::create_dir(output_path).expect("Could not create output directory");
     }
@@ -118,26 +123,26 @@ pub fn generate(module: &Module, context: &mut Context) {
     assert!(flint_config.is_object());
     let llc_path = flint_config
         .entries()
-        .find(|(key, _)| *key == "llcPath")
-        .expect("No llcPath in flint config")
+        .find(|(key, _)| *key == JSON_LCC_KEY)
+        .expect("No llc in flint config")
         .1
         .as_str()
-        .expect("Could not find llcPath in flint config");
+        .expect("Could not find llc in flint config");
     assert!(!llc_path.is_empty());
 
     let wasm_ld_path = flint_config
         .entries()
-        .find(|(key, _)| *key == "wasm-ldPath")
-        .expect("No wasm-ldPath in flint config")
+        .find(|(key, _)| *key == JSON_WASM_LD_KEY)
+        .expect("No wasm-ld in flint config")
         .1
         .as_str()
         .expect("Could not find wasm-ld path in flint config");
     assert!(!wasm_ld_path.is_empty());
 
     for contract in ewasm_contracts.iter() {
-        let tmp_path = Path::new("tmp");
+        let tmp_path = Path::new(TEMPORARY_DIRECTORY_NAME);
         if !tmp_path.exists() {
-            fs::create_dir(tmp_path).expect("Could not create tmp directory");
+            fs::create_dir(tmp_path).expect("Could not create temporary directory");
         }
 
         let file_name = contract.contract_declaration.identifier.token.as_str();
@@ -145,7 +150,7 @@ pub fn generate(module: &Module, context: &mut Context) {
 
         // Create LLVM file
         create_and_write_to_file(
-            Path::new(get_path("tmp", "ll").as_str()),
+            Path::new(get_path(TEMPORARY_DIRECTORY_NAME, "ll").as_str()),
             &*generate_llvm(contract).as_bytes(),
         )
         .expect("Could not create file");
@@ -155,7 +160,7 @@ pub fn generate(module: &Module, context: &mut Context) {
             .arg("-O3")
             .arg("-march=wasm32")
             .arg("-filetype=obj")
-            .arg(get_path("tmp", "ll"))
+            .arg(get_path(TEMPORARY_DIRECTORY_NAME, "ll"))
             .status()
             .expect("Could not compile to WASM");
 
@@ -165,15 +170,15 @@ pub fn generate(module: &Module, context: &mut Context) {
             .arg("--export-all")
             .arg("--allow-undefined")
             .arg("-o")
-            .arg(get_path("tmp", "wasm"))
-            .arg(get_path("tmp", "o"))
+            .arg(get_path(TEMPORARY_DIRECTORY_NAME, "wasm"))
+            .arg(get_path(TEMPORARY_DIRECTORY_NAME, "o"))
             .status()
             .expect("Could not link externally defined methods");
 
         // The following only exists so that we can inspect LLVM output and wasm files as wat files
         // while developing, and should be removed. TODO
         let wasm =
-            fs::read(Path::new(get_path("tmp", "wasm").as_str())).expect("Could not read wasm");
+            fs::read(Path::new(get_path(TEMPORARY_DIRECTORY_NAME, "wasm").as_str())).expect("Could not read wasm");
         let mut as_wat = wasm2wat(wasm).expect("Could not convert wasm to wat");
 
         // Shift final module closing curly brace onto its own line so it is not removed when trimming exports
@@ -188,39 +193,39 @@ pub fn generate(module: &Module, context: &mut Context) {
             .collect::<String>();
 
         create_and_write_to_file(
-            Path::new(get_path("tmp", "wat").as_str()),
+            Path::new(get_path(TEMPORARY_DIRECTORY_NAME, "wat").as_str()),
             &as_wat.as_bytes(),
         )
-        .expect("Could not create tmp wat file");
+            .expect("Could not create temporary wat file");
 
         let post_processed_wasm =
             wat2wasm(as_wat.as_bytes()).expect("Could not convert wat to wasm");
         create_and_write_to_file(
-            Path::new(get_path("output", "wasm").as_str()),
+            Path::new(get_path(OUTPUT_DIRECTORY_NAME, "wasm").as_str()),
             &post_processed_wasm,
         )
         .expect("Could not write to output wasm file");
 
         // TODO remove this for the final release, as we do not need to give wat and wasm files
         fs::copy(
-            Path::new(get_path("tmp", "wat").as_str()),
-            Path::new(get_path("output", "wat").as_str()),
+            Path::new(get_path(TEMPORARY_DIRECTORY_NAME, "wat").as_str()),
+            Path::new(get_path(OUTPUT_DIRECTORY_NAME, "wat").as_str()),
         )
-        .expect("Could not copy wat file from tmp to output");
+            .expect("Could not copy wat file from temporary dir to output dir");
 
         // TODO remove this for the final release, but for testing the LLVM, we need to be able to read the LLVM files
         fs::copy(
-            Path::new(get_path("tmp", "ll").as_str()),
-            Path::new(get_path("output", "ll").as_str()),
+            Path::new(get_path(TEMPORARY_DIRECTORY_NAME, "ll").as_str()),
+            Path::new(get_path(OUTPUT_DIRECTORY_NAME, "ll").as_str()),
         )
-        .expect("Could not copy wat file from tmp to output");
+            .expect("Could not copy ll file from temporary dir to output dir");
 
         // Delete all tmp files
-        fs::remove_dir_all(tmp_path).expect("Could not remove tmp directory");
+        fs::remove_dir_all(tmp_path).expect("Could not remove temporary directory");
 
         // Generate the ABI
         create_and_write_to_file(
-            Path::new(get_path("output", "json").as_str()),
+            Path::new(get_path(OUTPUT_DIRECTORY_NAME, "json").as_str()),
             &*generate_abi(&contract.contract_behaviour_declarations).as_bytes(),
         )
         .expect("Could not generate abi file");
